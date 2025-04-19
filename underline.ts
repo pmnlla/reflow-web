76d910b0a6bd4497d87b937ab4633b8ff5377f6a
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-04-19 15:11:12 -0400

    fix: asdjnkiah ks

--- underline.ts
+++ underline.ts
@@ -1,6 +1,7 @@
 import {visit} from "unist-util-visit";
 
 const underlinePlugin = () => { // transforms __underline__ to <u>underline</u>
+  // @ts-expect-error - too lazy to find types whatever
   function transformer(tree, file) {
     const source = file.value;
     
